4fc8cbefcf6440b5e05dee1e049a67cdaa310059
Lisa DN <dorvillelisa17@gmail.com>
2022-09-20 11:34:32 +0200

    mise en place page produit

--- front/js/product.js
+++ front/js/product.js
@@ -27,7 +27,7 @@ fetch("http://localhost:3000/api/products/" + id)
                     //continuer injection prix description
                     //ajouter ecouteur evenement sur bouton ajouter au panier
                     //vérifier couleur et quantité bien sélectionner
-                    // créer un nouveau script + creer fonction (rajouter dans product.html) ne pas oublier :appeler dans script.js(add to cart)
+                    //créer un nouveau script + creer fonction (rajouter dans product.html) ne pas oublier :appeler dans script.js(add to cart)
                 })
                 .catch(function (err) {
                     console.log(err)
